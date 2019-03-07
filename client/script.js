d49b7f394f39d528da83dfa0a3b52c4530b6d14d
iasjem <jemimah.beryl.sai@gmail.com>
2019-03-08 01:59:52 +0800

    Add features to save and clear sessions

--- client/script.js
+++ client/script.js
@@ -2,6 +2,8 @@ var editor = document.querySelector('#editor');
 var previewer = document.querySelector('#previewer');
 var wordCount = document.querySelector('#word-count');
 var characterCount = document.querySelector('#character-count');
+var btnSaveSession = document.querySelector('#save-session');
+var btnClearSession = document.querySelector('#clear-session');
 
 var wordLength = function(value) {
     return value === "" ? 0 : value.split(" ").length;
@@ -11,23 +13,38 @@ var characterLength = function(value) {
     return value.replace(/\s/g, '').length;
 };
 
-var output = function(editor) {
-    let md = editor.value;
+var output = function(md) {
+    editor.innerHTML = md;
+    previewer.innerHTML = marked(md); 
     wordCount.innerHTML = wordLength(md);
-    characterCount.innerHTML = characterLength(md);
-    previewer.innerHTML = marked(md);    
+    characterCount.innerHTML = characterLength(md);   
+};
+
+var loadSession = function(value) {
+    var data = sessionStorage.getItem('md');
+    let md = data !== null ? data : value;
+    output(md);
 };
 
 editor.addEventListener('input', function() {
     this.onkeydown = null;
-    output(editor);
+    output(editor.value);
 });
 
 // fallback if oninput event is not supported
 editor.addEventListener('onkeydown', function() {
-    output(editor);
+    output(editor.value);
+});
+
+btnSaveSession.addEventListener('click', function() {
+    sessionStorage.setItem('md', editor.value);
+});
+
+btnClearSession.addEventListener('click', function() {
+    sessionStorage.clear();
+    location.reload();
 });
 
 {
-    output(editor);
+    loadSession(editor.value);
 }
